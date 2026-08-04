6c4e9968dfbf28b430970862601ecc55fd8434ff
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-04 15:29:54 +0530

    initial commit

---
+++ instance1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SG_ID="sg-0df39c725f035792e"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do 
+    aws runn ec2-instances \
+    --image-ids $AMI_ID \
+    --security-group-ids $SG_ID \
+    --instance-type t3.micro \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0],PrivateIpAddress'
+    --output text
+
+done
