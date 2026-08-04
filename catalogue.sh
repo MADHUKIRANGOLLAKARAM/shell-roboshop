71db60415cc2ec3f7c89278cc18dc030785ca027
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-04 19:10:03 +0530

    initial commit

---
+++ catalogue.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+user_id=$(id -u)
+$LOGS_FOLDER="/var/log/shell-roboshop"
+mkdir -p $LOGS_FOLDER
+$LOG_FILE="$LOG_FOLDER/$0.log"
+
+if [ $user_id -ne 0 ]; then
+    echo "please enter root user environment..."  | tee -a $LOG_FILE
+    exit 1
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 is failure.."  | tee -a $LOG_FILE
+    else
+        echo "$2 is success..."  | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>> $LOG_FILE
+validate $? "disable default version..."
