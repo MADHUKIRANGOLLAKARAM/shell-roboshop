b94d6546dcc665605d1cc7c74a2f76d805b7b395
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-04 16:15:39 +0530

    changed

--- instance3.sh
+++ instance3.sh
@@ -13,7 +13,7 @@ echo "creating $instance instance"
     --query 'Instances[0].InstanceId' \
     --output text)
 
-if [ $instance =="frontend" ]; then
+if [ $instance == "frontend" ]; then
   IP=$( aws ec2 describe-instances \
     --instance-ids $Instance_id \
     --query 'Reservations[].Instances[].PublicIpAddress' \
