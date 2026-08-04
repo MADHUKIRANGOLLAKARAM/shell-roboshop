587a2cd0a243c391d6ba8e8154c698817f3f1e5c
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-04 23:05:58 +0530

    initial commit

--- frontend.sh
+++ frontend.sh
@@ -3,14 +3,14 @@
 user_id=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 mkdir -p $LOGS_FOLDER
-LOG_FILE="$LOGSFOLDER/s0.log"
+LOG_FILE="$LOGS_FOLDER/s0.log"
 
 if [ $user_id -ne 0 ]; then
     echo "ypu are not an user"
     exit 1
-
+fi
 validate(){
-    if [ $! -ne 0 ]; then
+    if [ $? -ne 0 ]; then
         echo "$2 is failure..." | tee -a $LOG_FILE
     else
         echo "$2 is success..." | tee -a $LOG_FILE
@@ -27,5 +27,6 @@ dnf install nginx -y
 validate $? "installing nodejs "
 
 systemctl enable nginx
+validate $? "enable nginx"
 systemctl start nginx
-validate $? "enable & start nginx"
+validate $? "start nginx"
