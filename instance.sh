65a72131eb3f0aa396963f708157713c1e262d62
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-04 17:42:09 +0530

    changed

--- instance.sh
+++ instance.sh
@@ -13,3 +13,26 @@ do
     --query 'Instances[0].PrivateIpAddress' \
     --output text
 done
+
+
+$LOGS_FOLDER="var/log/shell-roboshop"
+mkdir -p $LOGS_FOLDER
+LOG_FILE="$LOGS_FOLDER/$0.log"
+
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+validate $? "copying mongodb repository.."
+
+dnf install mongodb-org -y
+validate $? "installing mongodb"
+
+systemctl enable mongod
+validate $? "enableing mongodb"
+
+systemctl start mongod
+validate $? "start mongodb "
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+validate $? "alowing all ports "
+
+systemctl restart mongod
+validate $? "reatarting mongodb"
--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 user-id=$(id -u)
-$LOGS_FOLDER="var/log/shell-roboshop"
-mkdir -p $LOGS_FOLDER
-LOG_FILE="$LOGS_FOLDER/$0.log"
 
 if [ $user-id -ne 0 ]; then
     echo "please enter root environment.."
@@ -18,20 +15,7 @@ validate(){
     fi
 }
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
-validate $? "copying mongodb repository.."
+
 
 dnf install mongodb-org -y
-validate $? "installing mongodb"
-
-systemctl enable mongod
-validate $? "enableing mongodb"
-
-systemctl start mongod
-validate $? "start mongodb "
-
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-validate $? "alowing all ports "
-
-systemctl restart mongod
-validate $? "reatarting mongodb"
+valiadte $? "installing mongo "
