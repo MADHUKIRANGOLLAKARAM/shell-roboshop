3fc95b30dcd88a4e71850c741f1945ce433b76ab
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-04 15:35:52 +0530

    changed

--- instance1.sh
+++ instance1.sh
@@ -5,7 +5,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 for instance in $@
 do 
     aws ec2 run-instances \
-    --image-ids $AMI_ID \
+    --image-id $AMI_ID \
     --security-group-ids $SG_ID \
     --instance-type t3.micro \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
