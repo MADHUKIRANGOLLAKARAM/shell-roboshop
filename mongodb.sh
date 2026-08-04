3f929911009e5b01b8fd4f866dc129ce701066cf
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-04 18:33:07 +0530

    changed

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 user_id=$(id -u)
+$LOGS_FOLDER="var/log/shell-roboshop"
+mkdir -p $LOGS_FOLDER
+$LOG_FILE="$LOGS_FOLDER/$0.log"
+
 
 if [ $user_id -ne 0 ]; then
     echo "please enter root environment.."
     exit 1
 fi
-
 
 validate(){
     if [ $1 -ne 0 ]; then
@@ -16,6 +19,20 @@ validate(){
 }
 
 
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+validate $? "copying mongo repo "
 
-dnf remove nginx -y
-validate $? "installing mongo "
+dnf install mongo-org -y
+validate $? "installing mongodb "
+
+systemctl enable mongod
+validate $? "enableing mongo "
+
+systemctl start mongod
+validate $? "starting mongodb "
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+validate $? "allowing all ports "
+
+systemctl restart mongod
+validate $? "restarting mongodb "
