6c901b04461c868a96b9002b45fd67078463fd8e
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-04 19:16:07 +0530

    initial commit

--- catalogue.sh
+++ catalogue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 user_id=$(id -u)
-$LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FOLDER="/var/log/shell-roboshop"
 mkdir -p $LOGS_FOLDER
-$LOG_FILE="$LOG_FOLDER/$0.log"
+LOG_FILE="$LOG_FOLDER/$0.log"
 
 if [ $user_id -ne 0 ]; then
     echo "please enter root user environment..."  | tee -a $LOG_FILE
