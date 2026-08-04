1b5868454c251d1adc1b52de1b39e20c41ff2627
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-04 19:32:10 +0530

    initial commit

--- catalogue.sh
+++ catalogue.sh
@@ -34,4 +34,5 @@ if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
 else
  echo "roboshop user is already exist skipping."
+fi
 validate $? "creating roboshop user is "
